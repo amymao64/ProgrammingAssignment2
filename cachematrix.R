4f45d4e97e489111c607fc50f8f397f629d380ec
amymao64 <ymao64@gmail.com>
2016-11-07 00:16:16 -0600

    Add my hw code
    add hw code without changing name of the file

--- cachematrix.R
+++ cachematrix.R
@@ -10,6 +10,40 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
+
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+
+##Function makeCacheMatrix returns a matrix which will be used as an input to cacheSolve function for the inversion of matrix
+##x is defined as a square invertible matrix
+makeCacheMatrix <- function(x = matrix()) {
+  
+  inv = NULL
+  set = function(y) {
+    # assign a value in an environment 
+    x <<- y
+    inv <<- NULL # to store inversion
+  }
+  get = function() x
+  setinv = function(inverse) inv <<- inverse 
+  getinv = function() inv
+  list(set=set, get=get, setinv=setinv, getinv=getinv)
+}
+## return the inverse of the original matrix 
+
+
+cacheSolve <- function(x) {
+ 
+  inv = x$getinv()
+  if (!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+
+  data = x$get()
+  inv = solve(data)
+  x$setinv(inv)
+  return(inv)
 }
